f19360ec8620b5b3184ddee6efbea278ffb28356
Jens Reimann <jreimann@redhat.com>
2020-03-26 16:24:05 +0100

    initial import

---
+++ hawkbit.cpp
@@ -0,0 +1,334 @@
+#include "hawkbit.h"
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+StaticJsonDocument<16*1024> doc;
+
+HawkbitClient::HawkbitClient(
+    WiFiClient& wifi,
+    const String& baseUrl,
+    const String& tenantName,
+    const String& controllerId,
+    const String &securityToken) :
+    _wifi(wifi),
+    _baseUrl(baseUrl),
+    _tenantName(tenantName),
+    _controllerId(controllerId),
+    _authToken("TargetToken " + securityToken)
+{
+}
+
+UpdateResult HawkbitClient::updateRegistration(const Registration& registration, const std::map<String,String>& data, MergeMode mergeMode, std::initializer_list<String> details)
+{
+    doc.clear();
+
+    switch(mergeMode) {
+        case MERGE:
+            doc["mode"] = "merge";
+            break;
+        case REPLACE:
+            doc["mode"] = "replace";
+            break;
+        case REMOVE:
+            doc["mode"] = "remove";
+            break;
+    }
+
+    doc.createNestedObject("data");
+    doc["data"]["mac"] = WiFi.macAddress();
+    for (const std::pair<String,String>& entry : data) {
+        doc["data"][String(entry.first)] = entry.second;
+    }
+
+    JsonArray d = doc["status"].createNestedArray("details");
+    for (auto detail : details) {
+        d.add(detail);
+    }
+    
+    doc["status"]["execution"] = "closed";
+    doc["status"]["result"]["finished"] = "success";
+
+    _http.begin(this->_wifi, registration.url());
+
+    _http.addHeader("Accept", "application/hal+json");
+    _http.addHeader("Content-Type", "application/json");
+    _http.addHeader("Authorization", this->_authToken);
+
+#if ARDUHAL_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_DEBUG
+    serializeJsonPretty(doc, Serial);
+#endif
+
+    String buffer;
+    size_t len = serializeJson(doc, buffer);
+
+    log_i("JSON - len: %d", len);
+
+    int code = _http.PUT(buffer);
+    log_i("Result - code: %d", code);
+
+    String resultPayload = _http.getString();
+    log_i("Result - payload: %s", resultPayload.c_str());
+
+    _http.end();
+
+    return UpdateResult(code);
+}
+
+State HawkbitClient::readState()
+{
+    _http.begin(this->_wifi, this->_baseUrl + "/" + this->_tenantName + "/controller/v1/" + this->_controllerId);
+
+    _http.addHeader("Authorization", this->_authToken);
+    _http.addHeader("Accept", "application/hal+json");
+
+    int code = _http.GET();
+    log_i("Result - code: %d", code);
+    String resultPayload = _http.getString();
+    log_i("Result - payload: %s", resultPayload.c_str());
+    if ( code == HTTP_CODE_OK ) {
+        DeserializationError error = deserializeJson(doc, resultPayload);
+        if (error) {
+            // FIXME: need a way to handle errors
+            throw 1;
+        }
+    }
+    _http.end();
+
+    String href = doc["_links"]["deploymentBase"]["href"] | "";
+    if (!href.isEmpty()) {
+        log_i("Fetching deployment: %s", href.c_str());
+        return State(this->readDeployment(href));
+    }
+
+    href = doc["_links"]["configData"]["href"] | "";
+    if (!href.isEmpty()) {
+        log_i("Need to register", href.c_str());
+        return State(Registration(href));
+    }
+
+    href = doc["_links"]["cancelAction"]["href"] | "";
+    if (!href.isEmpty()) {
+        log_i("Fetching cancel action: %s", href.c_str());
+        return State(this->readCancel(href));
+    }
+
+    log_i("No update");
+    return State();
+}
+
+std::map<String,String> toMap(const JsonObject& obj) {
+    std::map<String,String> result;
+    for (const JsonPair& p: obj) {
+        if (p.value().is<char*>()) {
+            result[String(p.key().c_str())] = String(p.value().as<char*>());
+        }
+    }
+    return result;
+}
+
+std::map<String,String> toLinks(const JsonObject& obj) {
+    std::map<String,String> result;
+    for (const JsonPair& p: obj) {
+        const char* key = p.key().c_str();
+        const char* value = p.value()["href"];
+        result[String(key)] = String(value);
+    }
+    return result;
+}
+
+std::list<Artifact> artifacts(const JsonArray& artifacts)
+{
+    std::list<Artifact> result;
+
+    for (JsonObject o : artifacts) {
+        Artifact artifact (
+            o["filename"],
+            o["size"] | 0,
+            toMap(o["hashes"]),
+            toLinks(o["_links"])
+        );
+        result.push_back(artifact);
+    }
+
+    return result;
+}
+
+std::list<Chunk> chunks(const JsonArray& chunks)
+{
+    std::list<Chunk> result;
+
+    for(JsonObject o : chunks)
+    {
+        Chunk chunk(
+            o["part"],
+            o["version"],
+            o["name"],
+            artifacts(o["artifacts"])
+            );
+        result.push_back(chunk);
+    }
+
+    return result;
+}
+
+Deployment HawkbitClient::readDeployment(const String& href)
+{
+    _http.begin(this->_wifi, href);
+
+    _http.addHeader("Authorization", this->_authToken);
+    _http.addHeader("Accept", "application/hal+json");
+
+    int code = _http.GET();
+    log_i("Result - code: %d", code);
+    String resultPayload = _http.getString();
+    log_i("Result - payload: %s", resultPayload.c_str());
+    if ( code == HTTP_CODE_OK ) {
+        DeserializationError error = deserializeJson(doc, resultPayload);
+        if (error) {
+            // FIXME: need a way to handle errors
+            throw 1;
+        }
+    }
+    _http.end();
+
+    String id = doc["id"];
+    String download = doc["deployment"]["download"];
+    String update = doc["deployment"]["update"];
+
+    return Deployment(id, download, update, chunks(doc["deployment"]["chunks"]));
+}
+
+Stop HawkbitClient::readCancel(const String& href)
+{
+    _http.begin(this->_wifi, href);
+
+    _http.addHeader("Authorization", this->_authToken);
+    _http.addHeader("Accept", "application/hal+json");
+
+    int code = _http.GET();
+    log_i("Result - code: %d", code);
+    String resultPayload = _http.getString();
+    log_i("Result - payload: %s", resultPayload.c_str());
+    if ( code == HTTP_CODE_OK ) {
+        DeserializationError error = deserializeJson(doc, resultPayload);
+        if (error) {
+            // FIXME: need a way to handle errors
+            throw 1;
+        }
+    }
+    _http.end();
+
+    String stopId = doc["cancelAction"]["stopId"] | "";
+    
+    return Stop(stopId);
+}
+
+String HawkbitClient::feedbackUrl(const Deployment& deployment) const
+{
+    return this->_baseUrl + "/" + this->_tenantName + "/controller/v1/" + this->_controllerId + "/deploymentBase/" + deployment.id() + "/feedback";
+}
+
+String HawkbitClient::feedbackUrl(const Stop& stop) const
+{
+    return this->_baseUrl + "/" + this->_tenantName + "/controller/v1/" + this->_controllerId + "/cancelAction/" + stop.id() + "/feedback";
+}
+
+template<typename IdProvider>
+UpdateResult HawkbitClient::sendFeedback(IdProvider id, const String& execution, const String& finished, std::initializer_list<String> details)
+{
+    doc.clear();
+
+    doc["id"] = id.id();
+    
+    JsonArray d = doc["status"].createNestedArray("details");
+    for (auto detail : details) {
+        d.add(detail);
+    }
+
+    doc["status"]["execution"] = execution;
+    doc["status"]["result"]["finished"] = finished;
+
+    _http.begin(this->_wifi, this->feedbackUrl(id));
+
+    _http.addHeader("Accept", "application/hal+json");
+    _http.addHeader("Content-Type", "application/json");
+    _http.addHeader("Authorization", this->_authToken);
+
+    String buffer;
+    size_t len = serializeJson(doc, buffer);
+
+    log_i("JSON - len: %d", len);
+    serializeJsonPretty(doc, Serial);
+
+    // FIXME: handle result
+    int code = _http.POST(buffer);
+    log_i("Result - code: %d", code);
+
+    String resultPayload = _http.getString();
+    log_i("Result - payload: %s", resultPayload.c_str());
+
+    _http.end();
+
+    return UpdateResult(code);
+}
+
+UpdateResult HawkbitClient::reportProgress(const Deployment& deployment, uint32_t done, uint32_t total, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        deployment,
+        "proceeding",
+        "none",
+        details
+    );
+}
+
+UpdateResult HawkbitClient::reportScheduled(const Deployment& deployment, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        deployment,
+        "scheduled",
+        "none",
+        details
+    );
+}
+
+UpdateResult HawkbitClient::reportResumed(const Deployment& deployment, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        deployment,
+        "resumed",
+        "none",
+        details
+    );
+}
+
+UpdateResult HawkbitClient::reportComplete(const Deployment& deployment, bool success, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        deployment,
+        "closed",
+        success ? "success" : "failure",
+        details
+    );
+}
+
+UpdateResult HawkbitClient::reportCancelAccepted(const Stop& stop, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        stop,
+        "canceled",
+        "none",
+        details
+    );
+}
+
+UpdateResult HawkbitClient::reportCancelRejected(const Stop& stop, std::initializer_list<String> details)
+{
+    return sendFeedback(
+        stop,
+        "rejected",
+        "none",
+        details
+    );
+}
---
+++ hawkbit.h
@@ -0,0 +1,289 @@
+#ifndef _HAWBIT_H_
+#define _HAWBIT_H_
+
+#include <vector>
+#include <utility>
+#include <WString.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <map>
+#include <list>
+
+class Artifact;
+class Chunk;
+class Deployment;
+class State;
+class UpdateResult;
+
+class UpdateResult {
+    public:
+        UpdateResult(uint32_t code) :
+            _code(code)
+        {
+        }
+
+        uint32_t code() const { return this->_code; }
+
+    private:
+        uint32_t _code;
+};
+
+class Artifact {
+    public:
+        Artifact(
+            const String& filename,
+            uint32_t size,
+            const std::map<String,String>& hashes,
+            const std::map<String,String>& links
+            ) :
+            _filename(filename),
+            _size(size),
+            _hashes(hashes),
+            _links(links)
+        {
+        }
+
+        const String& filename() const { return _filename; }
+        const uint32_t size() const { return _size; }
+        const std::map<String,String>& hashes() const { return _hashes; }
+        const std::map<String,String>& links() const { return _links; }
+
+        void dump(Print& out, const String& prefix = "") const {
+            out.printf("%s%s %u\n", prefix.c_str(), this->_filename.c_str(), this->_size);
+            out.printf("%sHashes\n", prefix.c_str());
+            for (std::pair<String,String> element : this->_hashes) {
+                out.printf("%s    %s = %s\n", prefix.c_str(), element.first.c_str(), element.second.c_str());
+            }
+            out.printf("%sLinks\n", prefix.c_str());
+            for (std::pair<String,String> element : this->_links) {
+                out.printf("%s    %s = %s\n", prefix.c_str(), element.first.c_str(), element.second.c_str());
+            }
+        }
+
+    private:
+        String _filename;
+        uint32_t _size;
+        std::map<String,String> _hashes;
+        std::map<String,String> _links;
+};
+
+class Chunk {
+    public:
+        Chunk(const String& part, const String& version, const String& name, const std::list<Artifact>& artifacts) :
+            _part(part),
+            _version(version),
+            _name(name),
+            _artifacts(artifacts)
+        {
+        }
+
+        const String& part() const { return _part; }
+        const String& version() const { return _version; }
+        const String& name() const { return _name; }
+
+        void dump(Print& out, const String& prefix = "") const {
+            out.printf("%s%s - %s (%s)\n", prefix.c_str(), this->_name.c_str(), this->_version.c_str(), this->_part.c_str());
+            for (Artifact a: this->_artifacts) {
+                a.dump(out, prefix + "    ");
+            }
+        }
+
+    private:
+        String _part;
+        String _version;
+        String _name;
+        std::list<Artifact> _artifacts;
+};
+
+class Deployment {
+    public:
+        Deployment() {
+        }
+
+        Deployment(const String& id, const String& download, const String& update, const std::list<Chunk>& chunks) :
+            _id(id),
+            _download(download),
+            _update(update),
+            _chunks(chunks)
+        {
+        }
+
+        const String& id() const { return _id; }
+
+        void dump(Print& out, const String& prefix = "") const {
+            out.printf("%sDeployment: %s\n", prefix.c_str(), this->_id.c_str());
+            out.printf("%s    Download: %s, Update: %s\n", prefix.c_str(), this->_download.c_str(), this->_update.c_str());
+            out.printf("%s    Chunks:\n", prefix.c_str());
+            String chunkPrefix = prefix + "        ";
+            for (Chunk c : this->_chunks) {
+                c.dump(out, chunkPrefix);
+            }
+            out.println();
+        };
+    private:
+        String _id;
+        String _download;
+        String _update;
+        std::list<Chunk> _chunks;
+};
+
+class Stop {
+    public:
+        Stop() {
+        }
+
+        Stop(const String&id) :
+            _id(id)
+        {}
+
+        const String& id() const { return this->_id; }
+
+        void dump(Print& out, const String& prefix = "") const
+        {
+            out.printf("%sStop: %s\n", prefix.c_str(), this->_id.c_str());
+        }
+    private:
+        String _id;
+};
+
+class Registration {
+    public:
+        Registration()
+        {
+        }
+
+        Registration(const String& url):
+            _url(url)
+        {
+        }
+
+        const String& url() const { return this->_url; }
+
+        void dump(Print& out, const String& prefix = "") const
+        {
+            out.printf("%sRegistration: %s\n", prefix.c_str(), this->_url.c_str());
+        }
+
+    private:
+        String _url;
+};
+
+class State {
+
+    public:
+
+        typedef enum { NONE, REGISTER, UPDATE, CANCEL } Type;
+
+        State() :
+            _type(State::NONE)
+        {
+        }
+
+        State(const Stop& stop) :
+            _type(State::CANCEL),
+            _stop(stop)
+        {
+        }
+
+        State(const Registration& registration) :
+            _type(State::REGISTER),
+            _registration(registration)
+        {
+        }
+
+        State(const Deployment& deployment) :
+            _type(State::UPDATE),
+            _deployment(deployment)
+        {
+        }
+
+        boolean is(Type type) const
+        {
+            return this->_type == type;
+        }
+
+        const Type type() const { return this->_type; }
+        const Deployment& deployment() const { return this->_deployment; }
+        const Stop& stop() const { return this->_stop; }
+        const Registration& registration() const { return this->_registration; }
+
+        void dump(Print& out, const String& prefix = "") const
+        {
+            switch (this->_type) {
+                case State::NONE:
+                    out.printf("%sState <NONE>\n", prefix.c_str());
+                    break;
+                case State::UPDATE:
+                    out.printf("%sState <UPDATE>\n", prefix.c_str());
+                    this->_deployment.dump(out, "    ");
+                    break;
+                case State::CANCEL:
+                    out.printf("%sState <CANCEL>\n", prefix.c_str());
+                    this->_stop.dump(out, "    ");
+                    break;
+                case State::REGISTER:
+                    out.printf("%sState <REGISTER>\n", prefix.c_str());
+                    this->_registration.dump(out, "    ");
+                    break;
+                default:
+                    out.printf("%sState <UNKNOWN>\n", prefix.c_str());
+                    break;
+            }
+        }
+
+    private:
+        Type _type;
+        Deployment _deployment;
+        Stop _stop;
+        Registration _registration;
+};
+
+class HawkbitClient {
+    public:
+
+        typedef enum { MERGE, REPLACE, REMOVE } MergeMode;
+
+        HawkbitClient(
+            WiFiClient &wifi,
+            const String& baseUrl,
+            const String& tenantName,
+            const String& controllerId,
+            const String& securityToken);
+
+        State readState();
+
+        UpdateResult reportProgress(const Deployment& deployment, uint32_t done, uint32_t total, std::initializer_list<String> details = {});
+
+        UpdateResult reportComplete(const Deployment& deployment, bool success, std::initializer_list<String> details = {});
+        
+        UpdateResult reportScheduled(const Deployment& deployment, std::initializer_list<String> details = {});
+        
+        UpdateResult reportResumed(const Deployment& deployment, std::initializer_list<String> details = {});
+        
+        UpdateResult reportCancelAccepted(const Stop& stop, std::initializer_list<String> details = {});
+        
+        UpdateResult reportCancelRejected(const Stop& stop, std::initializer_list<String> details = {});
+
+        UpdateResult updateRegistration(const Registration& registration, const std::map<String,String>& data, MergeMode mergeMode = REPLACE, std::initializer_list<String> details = {});
+
+    private:
+        WiFiClient&_wifi;
+    
+        HTTPClient _http;
+
+        String _baseUrl;
+        String _tenantName;
+        String _controllerId;
+        String _authToken;
+
+        Deployment readDeployment(const String& href);
+        Stop readCancel(const String& href);
+
+        String feedbackUrl(const Deployment& deployment) const;
+        String feedbackUrl(const Stop& stop) const;
+
+        template<typename IdProvider>
+        UpdateResult sendFeedback(IdProvider id, const String& execution, const String& finished, std::initializer_list<String> details );
+};
+
+#endif // _HAWBIT_H_
